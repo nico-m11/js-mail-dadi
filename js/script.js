9ea9d6de8957d7fd9dcdef9d6060b2d67aa6dea5
Nicola <nicolamelito.11@gmail.com>
2020-10-22 18:53:14 +0200

    aggiunto style alla pagina

--- js/script.js
+++ js/script.js
@@ -1,18 +1,27 @@
 
 
-  var mailList = ["email1", "email2", "email3", "email4"];
+  var mailList = ["nicola@gmail.com", "melito@gmail.com", "prof@gmail.com", "boolean@gmail.com"];
   var email = prompt(" Ciao Inserisci la tua Email");
   var messaggio = document.getElementById("verifica");
   var emailTrovata = false;
 
  for (var i = 0; i < mailList.length; i++) {
   if (mailList[i] === email) {
-    console.log("ciao");
-  } else {
-    console.log("registrati");
+    emailTrovata = true;
   }
 }
-document.getElementById("verifica").innerHTML = messaggio;
+
+if (emailTrovata === false) {
+  console.log('errore');
+  document.getElementById("verifica").innerHTML = 'Non sei registrato';
+
+} else {
+  console.log('ok');
+  document.getElementById("verifica").innerHTML = 'Accesso Eseguito';
+
+}
+
+
 
 // secondo esercizio
 
@@ -22,14 +31,14 @@ var btnG = document.getElementById('btnG');
 var risultato = document.getElementById('risultato');
 
 btnG.addEventListener("click", function () {
-  document.getElementById('puno').innerHTML = Math.floor(Math.random("") * 5 + 1);
-  document.getElementById('pdue').innerHTML = Math.floor(Math.random("") * 5 + 1);
+  document.getElementById('puno').innerHTML = Math.floor(Math.random() * 5 + 1);
+  document.getElementById('pdue').innerHTML = Math.floor(Math.random() * 5 + 1);
 
   if (parseInt(puno.innerHTML) > parseInt(pdue.innerHTML)) {
-    risultato.innerHTML = "You WIN Player One"
+    risultato.innerHTML = "You WIN Player One";
   } else if (parseInt(puno.innerHTML) < parseInt(pdue.innerHTML)) {
-    risultato.innerHTML = "You WIN Player Due"
+    risultato.innerHTML = "You WIN Player Due";
   } else {
-    risultato.innerHTML = "Pareggio"
+    risultato.innerHTML = "Pareggio";
   }
 })

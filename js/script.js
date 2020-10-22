db540b94b94a6738652ad4993e53fb5ebfb4516a
Nicola <nicolamelito.11@gmail.com>
2020-10-22 17:39:58 +0200

    fatto secondo esercizio

--- js/script.js
+++ js/script.js
@@ -13,3 +13,23 @@
   }
 }
 document.getElementById("verifica").innerHTML = messaggio;
+
+// secondo esercizio
+
+var puno = document.getElementById('puno');
+var pdue = document.getElementById('pdue');
+var btnG = document.getElementById('btnG');
+var risultato = document.getElementById('risultato');
+
+btnG.addEventListener("click", function () {
+  document.getElementById('puno').innerHTML = Math.floor(Math.random("") * 5 + 1);
+  document.getElementById('pdue').innerHTML = Math.floor(Math.random("") * 5 + 1);
+
+  if (parseInt(puno.innerHTML) > parseInt(pdue.innerHTML)) {
+    risultato.innerHTML = "You WIN Player One"
+  } else if (parseInt(puno.innerHTML) < parseInt(pdue.innerHTML)) {
+    risultato.innerHTML = "You WIN Player Due"
+  } else {
+    risultato.innerHTML = "Pareggio"
+  }
+})

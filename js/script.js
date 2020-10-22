140433b745ec1e784ce926c73aebbb972997445d
Nicola <nicolamelito.11@gmail.com>
2020-10-22 17:16:55 +0200

    modificato prima parte della mail

--- js/script.js
+++ js/script.js
@@ -1,18 +1,15 @@
-var btn = document.getElementById("btn");
-var mail = ["email1", "email2", "email3", "email4",];
-var email = document.getElementById("email");
-var messaggio =document.getElementById("verifica");
 
-btn.addEventListener("click", function () {
-  email === mail.value;
 
- for (var i = 0; i < mail.lenght; i++){
+  var mailList = ["email1", "email2", "email3", "email4"];
+  var email = prompt(" Ciao Inserisci la tua Email");
+  var messaggio = document.getElementById("verifica");
+  var emailTrovata = false;
 
-  if (mail[i] == email) {
-    i = mail;
-    messaggio.innerHTML = "Email Verificata"
-  } else if (mail[i] != email) {
-    messaggio.innerHTML = "Email non Verificata"
+ for (var i = 0; i < mailList.length; i++) {
+  if (mailList[i] === email) {
+    console.log("ciao");
+  } else {
+    console.log("registrati");
   }
 }
-})
+document.getElementById("verifica").innerHTML = messaggio;
